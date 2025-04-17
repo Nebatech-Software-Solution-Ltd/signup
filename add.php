e93ba590e1e38177b738ee11e15d621b0ab05ec2
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-04-17 16:19:04 +0000

    Update add.php

--- add.php
+++ add.php
@@ -1,6 +1,6 @@
 <?php
-$num1 = "25";
-$num2 = "5";
+$num1 = 25;
+$num2 = 5;
 $operation = 'add'; // Change this to 'subtract', 'multiply', or 'divide' to test other operations
 $result = $num1 + $num2; // Default to addition
 echo "The result of adding $num1 and $num2 is: $result\n";

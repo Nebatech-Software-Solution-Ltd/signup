cbb686b8dc34519b2716ee4d48c7b3c5e0d14d2a
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-04-17 16:44:22 +0000

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,5 @@
 <?php
-$num1 = 25;
-$num2 = 5;
-$operation = 'add'; // Change this to 'subtract', 'multiply', or 'divide' to test other operations
-$result = $num1 + $num2; // Default to addition
-echo "The result of adding $num1 and $num2 is: $result\n";
+$FirstName = "Abdul-Hafiz";
+$Surnane = "Yussif";
+echo "Hello $FirstName $Surnane!";
 ?>
